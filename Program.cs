4356fa3353c617ad26d29c88f835237ffc7027de
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-09-01 19:02:06 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinaryReaderWriterDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
